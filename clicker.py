47685c91c70b384af8013c8451c86090ca6462b0
chiiriill <chiiriill@yandex.ru>
2023-01-09 00:46:27 +0300

    Add clicker file and readme

---
+++ clicker.py
@@ -0,0 +1,16 @@
+"""
+    Module clicker: when the user presses "s" module makes 10 clicks per seconds.
+    To stop you need to press "s" again.
+"""
+
+
+def clicker():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
