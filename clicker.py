55c182fadb60b9a966251d658417634971d47b16
chiiriill <chiiriill@yandex.ru>
2023-01-09 01:01:08 +0300

    Add a function that clicks

--- clicker.py
+++ clicker.py
@@ -2,10 +2,23 @@
     Module clicker: when the user presses "s" module makes 10 clicks per seconds.
     To stop you need to press "s" again.
 """
+from pynput.mouse import Controller, Button
+import time
+
+MOUSE = Controller()
+CLICK: bool = False
 
 
-def clicker():
-    pass
+def clicker() -> None:
+    """
+    A function that makes clicks if the user has started the process
+
+    return: None
+    """
+    while True:
+        if CLICK:
+            MOUSE.click(Button.left)
+            time.sleep(0.1)
 
 
 def main():
